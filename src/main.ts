21bd3dbf5c3d5895a54db6f37528c03020dfe051
anhtuan <hoangthoanhtuan@gmail.com>
2018-01-22 16:58:17 +0700

    apply first component

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-// import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
-// import {AppModule} from "./appModule";
-// platformBrowserDynamic().bootstrapModule(AppModule);
+
+import {NgModule} from "@angular/core"
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {AppModule} from "./modules/app/appModule";
+platformBrowserDynamic().bootstrapModule(AppModule);
 console.log("Main file was bootstraped");
---
+++ src/modules/app/defaultPage.ts
@@ -0,0 +1,8 @@
+
+import {Component} from "@angular/core";
+@Component({
+    selector:"default-page",
+    template:"<div>hello Tuan</div>"
+    
+})
+export class DefaultPage{}
